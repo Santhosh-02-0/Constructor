0275519275e4041b50c40c43a7dbce68a2cbf341
Santhosh G <santhoshg0200@gmail.com>
2024-09-24 10:32:22 +0530

    Add files via upload

---
+++ EnumProgram.java
@@ -0,0 +1,15 @@
+package ConstructorPack;
+
+//Enumeration (It is a case used in the Switch)
+
+//Enum will be used outside the class
+enum Month{JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT,NOV,DEC;}
+public class EnumProgram {
+	
+
+		public static void main(String args[]) {
+
+			Month m=Month.MAY;
+			System.out.println(m);
+}
+}
